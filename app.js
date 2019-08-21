f388788f89f63f45ef1e03834b6a71cb5c7235c1
Антон Наумович <antonnaumovich1992@gmail.com>
2019-08-21 13:44:15 +0300

    2

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-// var person = {
-//     firstName: "Jim",
-//     introduce: function(){
-//         console.log("Hi, I'm " + this.firstName);
-//     }
-// };
- 
-// person.introduce();
-// Outputs: Hi, I'm Jim
- 
-// setTimeout(person.introduce.bind(person), 50);
-let a = delayedOutput('Карл у Клары украл кораллы, а Клара у Карла украла кларнет', 2, 5, 4, 2, 1);
+
+let a = delayedOutput('1 2 3 4 5 6 7 8 9 10', 2, 2, 5, 4, 3, 5, 10, 12, 4, 5);
 // let b = delayedOutput('Карл у Клары', 2, 5, 4, 2, 1);
 
 // console.log("begin");
@@ -27,7 +17,28 @@ let a = delayedOutput('Карл у Клары украл кораллы, а Кл
 // }, 500);
 // console.log("end");
 
+// function sortObject(obj) {
+//     var arr = [];
+//     for (var prop in obj) {
+//         if (obj.hasOwnProperty(prop)) {
+//             arr.push({
+//                 'key': prop,
+//                 'value': obj[prop]
+//             });
+//         }
+//     }
+//     arr.sort(function(a, b) { return a.value - b.value; });
+//     return arr;
+// };
   
+// function sortObjByValue(list){
+//     var sortedObj = {}
+//     Object.keys(list)
+//      .map(key => [key, list[key]])
+//      .sort((a,b) => a[1] > b[1] ? 1 : a[1] < b[1] ? -1 : 0)
+//      .forEach(data => sortedObj[data[0]] = data[1]);
+//     return sortedObj;
+//    }
 
 function delayedOutput(words, ...args){
     let arrFromString = words.split(' ')
@@ -41,11 +52,11 @@ function delayedOutput(words, ...args){
             return this;
           };  
         arrFromArgs = arrFromArgs.addArg( arrFromArgs[arrFromArgs.length - 1], arrFromString.length - arrFromArgs.length);
-        // console.log(arrFromArgs);   
+        console.log(arrFromArgs);   
     } else {
         arrFromArgs = arrFromArgs.slice(0, arrFromString.length);
-        // console.log(arrFromArgs);
-    }
+        console.log(arrFromArgs);
+    };
 // var result = arrFromArgs.map(function(row) {
 //   return arrFromArgs.reduce(function(result, field, index) {
 //     result[arrFromString[index]] = field;
@@ -62,8 +73,32 @@ function delayedOutput(words, ...args){
     };
 
   console.log(Arr2object(arrFromString, arrFromArgs));
+let list = Arr2object(arrFromString, arrFromArgs);
 
-// console.log(result);
+
+for (let key in list) {
+    setTimeout( function(){
+        console.log(key);
+    }, list[key]*1000);
+};
+
+/*https://habr.com/ru/post/116581/*/
+// let sorted = sortObject(list);
+
+// let entries = Object.entries(objFromFun);
+// let sorted = entries.sort((a, b) => a[1] - b[1]);
+
+// let sorted = sortObjByValue(list)
+
+// console.log(sorted)
+
+// for (let key in sorted){
+//     setTimeout(function () {
+// 	console.log(key);
+//     }, (sorted[key]));
+// }
+
+
 };
 //     let keys = words.split(' ');
 //     let values = args;
